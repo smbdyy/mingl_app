c455cf718cf92bd89176fb51fc7ce32f30841e41
Ilya <ividivi67@gmail.com>
2026-01-26 16:35:29 +0300

    *

--- lib/core/models/account.dart
+++ lib/core/models/account.dart
@@ -1,11 +1,13 @@
 class Account {
   final int id;
+  final String email;
 
-  Account({required this.id});
+  Account({required this.id, required this.email});
 
   factory Account.fromJson(Map<String, dynamic> json) {
     return Account(
-      id: json['accountId'] as int,
+      id: json['id'] as int,
+      email: json['email'] as String
     );
   }
 }
